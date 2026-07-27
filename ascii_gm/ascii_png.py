56844779900a68fc76bd11b917aab36219bfd12b
NoRaincheck <2498638+NoRaincheck@users.noreply.github.com>
2026-07-28 06:24:32 +1000

    Fix up the ascii generation so that the spacing is corrected

--- ascii_gm/ascii_png.py
+++ ascii_gm/ascii_png.py
@@ -7,8 +7,8 @@ from .theme import PNG_PALETTE, TEMPLATE_PATH, get_field_color
 base_path = Path(__file__).parent
 
 template = TEMPLATE_PATH.read_text()
-font_type = str(base_path.joinpath("FiraMono-Regular.ttf"))
-font_size = 14
+font_type = str(base_path.joinpath("FiraCode-Regular.ttf"))
+font_size = 16
 font = ImageFont.truetype(font_type, size=font_size)
 
 
@@ -18,7 +18,7 @@ def create_card(
     text_color=(76, 79, 105),
     bg_color=(239, 241, 245),
 ):
-    char_width = 10
+    char_width = int(font.getlength(" "))
     char_height = 18
     padding = 2
 
@@ -37,27 +37,26 @@ def create_card(
     for li in range(len(card_lines)):
         card_line = card_lines[li]
         templ_line = template_lines[li] if li < len(template_lines) else ""
+
         x = 0
-
         for ci in range(len(card_line)):
-            char = card_line[ci]
-            base_char = templ_line[ci] if ci < len(templ_line) else char
-
-            if char != base_char:
+            base_char = templ_line[ci] if ci < len(templ_line) else card_line[ci]
+            if card_line[ci] != base_char:
                 color = get_field_color(li, ci, card_lines, PNG_PALETTE)
                 if color is not None:
                     draw.rectangle(
-                        (x - padding, y, x + char_width, y + char_height),
+                        (x - padding, y, x + char_width + padding, y + char_height),
                         fill=color,
                     )
+            x += char_width
 
-            draw.text(
-                (x, y),
-                char,
-                font=font,
-                fill=text_color if char == base_char else bg_color,
-            )
+        draw.text((0, y), card_line, font=font, fill=text_color)
 
+        x = 0
+        for ci in range(len(card_line)):
+            base_char = templ_line[ci] if ci < len(templ_line) else card_line[ci]
+            if card_line[ci] != base_char:
+                draw.text((x, y), card_line[ci], font=font, fill=bg_color)
             x += char_width
 
         y += char_height
